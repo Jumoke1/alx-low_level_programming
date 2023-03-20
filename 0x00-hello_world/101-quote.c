036423a444800b575cac4ec0f770945435fcb815
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-20 16:22:00 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+/**main c programming 
+  * Return 0
+ */
+int write(int filedes, const char *buf, unsigned int nbyte); 
+int main( int argc, char** argv ) 
+{ 
+   write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
+ 
+   return (1); 
+} 
+ 
