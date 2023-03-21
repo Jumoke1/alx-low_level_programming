c28a040399902ca629d696f96d08224901812b41
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-21 05:27:23 -0700

    putchar

---
+++ 0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - c programming
+ * Return 0
+ */
+int main(void)
+{
+putchar("_putchar");
+putchar('\n')
+/*Return 0 to indicate successful execution*/
+return (0);
+}
