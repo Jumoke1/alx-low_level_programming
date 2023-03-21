c4020b91eef71b4cc75e736c6913a903ef0c981e
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-21 04:59:33 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h> 
-/**main c programming 
-  * Return 0
+#include <stdio.h>
+/**
+ * main - c programming
+ * Return 0
  */
-int write(int filedes, const char *buf, unsigned int nbyte); 
-int main( int argc, char** argv ) 
-{ 
-   write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
- 
-   return (1); 
-} 
- 
+int main(void)
+{
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+
+return (1);
+}
