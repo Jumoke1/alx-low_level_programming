7cce8038a0b29dac09369c20413fecc7d6222146
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-22 16:33:54 -0700

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,7 +8,7 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar('+';)
+_putchar('+');
 return (1);
 }
 else if (n == 0)
@@ -18,6 +18,7 @@ return (0);
 }
 else if (n < 0)
 {
-_putchar(-);
+_putchar('-');
 return (-1);
+}
 }
