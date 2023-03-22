651e0134e17cab05feb49988f7fb8e064bc7db90
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-22 13:06:30 -0700

    print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print alphabetx10
+ */
+void print_alphabet_x10(void)
+{
+char p;
+int i;
+for (i = 0; i <= 10; i++)
+{
+for (p = 'a'; p <= 'z' p++)
+{
+_putchar(p);
+}
+_putchar('\n');
+}
+}
