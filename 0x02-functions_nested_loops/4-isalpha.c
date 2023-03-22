eda8ec5cb43c99883d27cf9294eebe3eb164ceda
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-22 15:31:58 -0700

    lowercase or uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,16 @@
 #include "main.h"
 /**
- * _isalpha - isalpha
+ * _isalpha - is alpha
  * @c: c is lowercase or uppercase
  * Return: 0 if c ia a letter, lowercase or uppercase otherwise 0
  */
 int _isalpha(int c)
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
