3512149916be4ceb13bfca3cc48bd2918adcc270
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-22 13:57:41 -0700

    lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _islower - int islower
+ * @c: c is lower case
+ * Return 0 otherwise return 1
+ */
+int_islower(int c)
+{
+if (c == "c")
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
