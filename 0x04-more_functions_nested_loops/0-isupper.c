b3cab1e4b352c9712192481ce2783ec009e01f94
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-23 09:10:03 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - isupper
+ * @c: is upper case
+ * Return: 1 if is c is uppercase otherwise return 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
