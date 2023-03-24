57ef0df3b4b4f14e671c6660df14fedfd24b3060
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-24 15:39:35 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,7 +6,6 @@
  */
 void print_line(int n)
 {
-
 if (n <= 0)
 {
 _putchar('\n');
@@ -14,7 +13,7 @@ _putchar('\n');
 else
 {
 int i;
-for (i = 0; i <= n; i++)
+for (i = 1; i <= n; i++)
 {
 _putchar('-');
 }
