29fd27796e5ad069119f25cecafb8ca1bbb56e68
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-24 16:25:29 -0700

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal
+ * @n: number of n value
+ */
+void print_diagonal(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
