91f239a64894176a95e2ecd0276bd49bf8c12534
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-27 15:34:19 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,13 @@
 #include "main.h"
 /**
-*  _puts - print string 
-* @str: values 
+*  _puts - print string
+* @str: values
 */
 void _puts(char *str)
 {
+while (str != '\0')
+{
+_putchar('str++');
+}
+_putchar('\n');
+}
