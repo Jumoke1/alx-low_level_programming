3edfa48014b206f00564e361e4f4eaf278dc9988
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-27 15:45:57 -0700

    print revers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,23 @@
+#include "main.h"
 /**
-* print_rev - Defends itself
-* @s: value to be reversed
-* Return: reversed length
+* print_rev - only go one way
+* @s: print
 */
 void print_rev(char *s)
+{
+int len = 0;
+
+while (*s != '\0')
+{
+len++;
+s++;
+}
+s--;
+while (len > 0)
+{
+_putchar(*s);
+s--;
+len--;
+}
+_putchar('\n');
+}
