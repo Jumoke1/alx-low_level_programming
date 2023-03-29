ca1b2b07c3ad1d957c724273c3a83a81aad82c06
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-29 13:24:53 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,9 +7,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-char *dest;
-char *src;
+int i;
+int j;
+i = 0;
 
-strcat(dest, src)
-_putchar(strcat)
+while (dest[i] != '\0')
+{
+i++
 }
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] != '\0';
+return (dest);
