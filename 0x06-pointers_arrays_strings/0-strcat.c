8566e74011d158d0455c302ba2a505f7bb2054fd
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-29 05:53:41 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two string
+ * @dest: string to be concatenated
+ * @src: string to be concatenated
+ * Return: pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+char *dest;
+char *src;
+
+strcat(dest, src)
+_putchar(strcat)
+}
