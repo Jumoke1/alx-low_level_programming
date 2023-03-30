26f8b2e70289987d8a5817a3a10184b0817b5e05
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-30 13:39:01 -0700

    capitalize string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,34 @@
 #include "main.h"
 /**
- * *cap_string - capitalize string 
- * @a: string 
+ * *cap_string - capitalize string
+ * @a: string
  * Return: string
  */
-char *cap_string(char *)
+char *cap_string(char *a)
+{
+int i;
+
+i = 0;
+
+while (!(a[i] >= 'a' && a[i] <= 'z'))
+{
+i++;
+if (a[i - 1] == ' ' ||
+	a[i - 1] == '\t' ||
+	a[i - 1] == '\n' ||
+	a[i - 1] == ',' ||
+	a[i - 1] == ';' ||
+	a[i - 1] == '.' ||
+	a[i - 1] == '!' ||
+	a[i - 1] == '?' ||
+	a[i - 1] == '"' ||
+	a[i - 1] == '(' ||
+	a[i - 1] == ')' ||
+	a[i - 1] == '{' ||
+	a[i - 1] == '}' ||
+	i == 0)
+a[i] = a[i] - 32;
+i++;
+}
+return (a);
+}
