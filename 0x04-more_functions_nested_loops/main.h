00cd900497d1c2f77fe3f4e796c5b16ecd13f920
Jumoke1 <kazeemjumoke12@gmail.com>
2023-03-31 10:02:28 -0700

    print num

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef main_h
+
 void _putchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,5 +11,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n)
+void print_number(int n);
+
 #endif
