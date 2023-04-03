54bcee74d0a55781f041538bfd975c7a0083406d
Jumoke1 <kazeemjumoke12@gmail.com>
2023-04-03 15:09:23 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strstr - finf occurence of substring
+ * @haystack: string
+ * @needle: pointer
+ * Return: substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (haystack != '\0'; haystack++)
+{
+char *p = haystack;
+char *j = needle;
+
+while (*p == *j && *p != '\0')
+{
+p++;
+j++;
+}
+if (*p == '\0')
+return (haystack);
+}
+return (0);
