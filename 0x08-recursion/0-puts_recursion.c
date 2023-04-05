2c33242d866d42187acdaf9f888c6861b2ec264b
Jumoke1 <kazeemjumoke12@gmail.com>
2023-04-05 11:27:43 -0700

    print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,16 @@
 /**
  * _puts_recursion - print a string
  * @s: string
- * Return string
+ * Return: string
  */
 
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
-_putchar(*s);
-_puts_recursion(s + 1);
-}
 _putchar('\n');
-return (0);
+return,
+}
+putchar(*s);
+_puts_recursion(s + 1);
 }
