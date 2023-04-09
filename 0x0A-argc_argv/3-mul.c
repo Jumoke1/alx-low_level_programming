5cabcb3e99f63702d3b970d652b91d9ec7904d65
Jumoke1 <kazeemjumoke12@gmail.com>
2023-04-09 16:04:35 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,21 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - program that mutiplies two num
  * @argc: argument count
  * @argv: argument vector
  * Return: (1);
  */
-int main(int argc, char *argv)
 
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
-
-	for (i = 1; i < argc; i++)
+	if (argc != 3)
 	{
-		for (j = 0; argv[i][j]; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("%s\n", "Error");
-				return (1);
-			}
-		}
-		sum += atoi(argv[i]);
+	printf("%s\n", "Error");
+	return (1);
 	}
-	printf("%i\n", sum);
+	printf("%i\n", (atoi(argv[1]) * atoi(argv[2])));
 	return (0);
 }
