6ac8d4c64f4153b05022749d733c1a82ae112f37
Jumoke1 <kazeemjumoke12@gmail.com>
2023-04-09 01:36:53 -0700

    print a name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print a name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: (0);
+ */
+int main(int argc, char *argv[])
+{
+/* suppress any warning agint the variable argc isnot used */
+(void)argc;
+/*%s is a format specifier for strings */
+/*print the first value in argv array*/
+printf("%s/n", argv[0]);
+
+return (0);
+}
