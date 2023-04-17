daaf0b45af3b1a4999f3a80dd4b331f37078c3ab
Jumoke1 <kazeemjumoke12@gmail.com>
2023-04-17 13:25:33 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,3 +13,14 @@ char *name;
 float age;
 char *owner;
 };
+/* initialize the struct dog */
+struct dog my_dog = {
+"nina",
+3.5,
+"jums"
+};
+printf("my dog name is %s\n", my_dog.name);
+printf("my dog is %.years old\n", my_dog.age);
+printf("my dog owner is %s\n", my_dog.age);
+
+#endif
