53299399879f4c8dd2d24600e0b7809cff40108a
Jumoke1 <kazeemjumoke12@gmail.com>
2023-04-17 16:21:59 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,7 @@ float age;
 char *owner;
 };
 /* typedef struct dog */
-typedef struct dog my_dog;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
