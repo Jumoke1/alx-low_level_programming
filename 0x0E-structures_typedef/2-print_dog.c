6dabe6a966be61b25cbbf139bde62bc9006c9553
Jumoke1 <kazeemjumoke12@gmail.com>
2023-04-17 17:22:58 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - print the struct dog
+ * @d: structure variable
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return
+if (d->name  == NULL)
+
+printf("name:(nill)\n");
+
+else 
+printf("Name:%s\n",d->name);
+
+if (d->age < 0)
+printf("age: (nill)\n");
+
+else 
+printf("Name:%f\n",d->age);
+
+if (d->owner == NULL)
+printf("Owner: (nill)\n");
+
+else 
+printf("owner:%s\n",d->owner);
+}
