4827e617db54144f1c58958f96790b25e34cef96
Jumoke1 <kazeemjumoke12@gmail.com>
2023-04-27 13:25:08 -0700

    list lent

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - the function
+ * @h: pointer to the head
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+        size_t c = 0;
+
+for (; h != NULL; h = h->next, c++)
+{
+        if (h->str == NULL)
+        printf("[0] (nil)\n");
+        else
+        printf("[%u] %s\n", h->len, h->str);
+}
+
+        return (c);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+/**
+ * list_len - the function
+ * @h: pointer to the head
+ *
+ * Return: c
+ */
+size_t list_len(const list_t *h)
+{
+        size_t c = 0;
+
+        for (; h != NULL; h = h->next)
+        {
+                c++;
+        }
+
+        return (c);
+}
