6d36bffceb74b32b862b8921f73ed0654ed2ecbb
Jumoke1 <kazeemjumoke12@gmail.com>
2023-04-27 16:14:47 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,19 +4,19 @@
  * print_list - the function
  * @h: pointer to the head
  *
- * Return: number of nodes
+ * Return: number of node
  */
 size_t print_list(const list_t *h)
 {
-        size_t c = 0;
+size_t c = 0;
 
 for (; h != NULL; h = h->next, c++)
 {
-        if (h->str == NULL)
-        printf("[0] (nil)\n");
-        else
-        printf("[%u] %s\n", h->len, h->str);
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
 }
 
-        return (c);
+return (c);
 }
