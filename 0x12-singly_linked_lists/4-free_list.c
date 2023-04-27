66450915c3f4178bdc6128a891f0b587ecd72c29
Jumoke1 <kazeemjumoke12@gmail.com>
2023-04-27 16:02:41 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - a function that free a list
+ * @head: member
+ */
+void free_list(list_t *head)
+{
+list_t *current;
+
+while (head != NULL)
+{
+current = head;
+head = head->next;
+free(current->str);
+free(current);
+}
+}
