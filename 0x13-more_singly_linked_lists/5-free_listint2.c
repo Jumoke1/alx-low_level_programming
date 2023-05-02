c78d971bfc9593fc785d2be7ce602699105a5cc3
Jumoke1 <kazeemjumoke12@gmail.com>
2023-05-02 01:08:06 +0100

    free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,16 +5,16 @@
  */
 void free_listint2(listint_t **head)
 {
-listint_t *temp;
+listint_t *t;
 
-        if (head == NULL)
-        return;
+if (head == NULL)
+return;
 
-        while (*head)
-        {
-                temp = (*head)->next;
-                free(*head);
-                *head = temp;
-        }
+while (*head)
+{
+t = (*head)->next;
+free(*head);
+*head = t;
+}
 *head = NULL;
 }
