f3798d1aabe7e1b5f32ea72bd64d15ad314a4742
Jumoke1 <kazeemjumoke12@gmail.com>
2023-05-06 00:35:21 +0100

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,7 @@ void print_binary(unsigned long int n)
 {
 int a;
 addup = 0;
-unsigned long  int presentbit;
+unsigned long int presentbit;
 
 for (a = 63; a >= 0; a--)
 {
@@ -20,8 +20,8 @@ _putchar('1');
 addup++;
 }
 else if (addup)
-_putchar('0);
+_putchar('0');
 }
-if(!addup)
+if (!addup)
 _putchar('0');
 }
