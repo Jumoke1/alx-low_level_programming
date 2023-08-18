c7dba11ce36d3259c15cd63da83fe8d6c24b681c
Jumoke1 <kazeemjumoke12@gmail.com>
2023-08-18 20:32:41 +0100

    print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * print_dlistint - function that prints a doubly linked list
+ * @h: pointer to the head of the list
+ * Return: number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t c = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n); 
+        h = h->next;
+        c++;
+    }
+
+    return (c);
+}
