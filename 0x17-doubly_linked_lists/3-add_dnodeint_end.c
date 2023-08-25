bfabcc7f487e54a939d055414c084f06aca0c6f5
Jumoke1 <kazeemjumoke12@gmail.com>
2023-08-25 01:22:40 +0100

    double linked list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,38 +1,32 @@
 #include "lists.h"
-#include <string.h>
 
 /**
- * add_dnodeint_end - function to add node at the end of a list
- * @n: integer
- * @head: a pointer
- * Return: address
+ * add_dnodeint_end - function to adds a new node at the end
+ * @head: pointer to the head
+ * @n: the integer
+ * Return: the address
  */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
+	dlistint_t *newnode1, *newnode2;
 
-dlistint_t *newnode1, *newnode2;
+	newnode1 = malloc(sizeof(dlistint_t));
+	if (newnode1 == NULL)
+		return (NULL);
 
-newnode1 = malloc(sizeof(dlistint_t));
-
-if (newnode == NULL)
-return (NULL);
-
-newnode1->n = n
-newnode1->next = NULL;
-
-if (*head == NUL)
-
-newnode1->prev = NULL;
-*head = newnode1;
-return (newnode1);
-
-newnode2 = *head;
-while (newnode2->next != NULL)
-newnode2 = newnode2->next;
-
-newnode2->next = newnode1;
-newnode1->prev = newnode2;
-
-return (newnode1);
+	newnode1->n = n;
+	newnode1->next = NULL;
+	if (*head == NULL)
+	{
+		newnode1->prev = NULL;
+		*head = newnode1;
+		return (newnode1);
+	}
+	newnode2 = *head;
+	while (newnode2->next != NULL)
+		newnode2 = newnode2->next;
+	newnode2->next = newnode1;
+	newnode1->prev = newnode2;
+	return (newnode1);
 }
