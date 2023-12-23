3afb6f83d9b22d97a33d430f26af9dbcccae4983
Jumoke1 <kazeemjumoke12@gmail.com>
2023-12-22 18:17:16 -0800

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,23 +8,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 unsigned long int i;
-hash_table_t *new_table
+hash_table_t *new_table;
 
 /* allocate memory for the new table */
-	* new_table = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t));
 /*return null if allocation was not successful */
 	if (new_table == NULL)
 		return (NULL);
 /*allocate memory for the array insidethehash table*/
 /* which size is (size) */
-	new_table->size = size
+	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 /* check if the allocation failed hence free the*/
 /* memory that was allocated to prevent memory leaks*/
 	if (new_table->array == NULL)
-
-		free(new_table);
-		return (NULL);
+	free(new_table);
+	return (NULL);
 
 /*intiallize array element to null */
 	for  (i = 0; i < size; i++)
