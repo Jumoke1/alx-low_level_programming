db5080612dbc015b6ca85ad9994048151f609d9e
Jumoke1 <kazeemjumoke12@gmail.com>
2024-04-10 16:29:40 -0700

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,11 +13,11 @@ int linear_search(int *array, size_t size, int value)
 	size i;
 
 	if (array == NULL)
-       		return (-1);
+		return (-1);
 
-	for (i =0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array"[ld%] = [d%]/n, i, arrray[i]);
+		printf("Value checked array[ld%] = [d%]/n", i, arrray[i]);
 		if (array[i] == value)
 			return (i);
 	}
